5ccd927443fa52fc29f8a579085111a2c21b4e83
srinivasrao452 <30620328+srinivasrao452@users.noreply.github.com>
2024-08-29 21:25:15 +0530

    Update forms.py for adding comment line

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+## This is a comment line from server side added
+
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
     class Meta:
